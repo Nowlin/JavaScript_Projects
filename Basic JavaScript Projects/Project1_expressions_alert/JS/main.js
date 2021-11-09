cf39470c92e5f3c93ecf987e94a0bb13821f9358
Nowlin <92047364+Nowlin@users.noreply.github.com>
2021-11-09 16:08:42 -0600

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!");
+document.write("How is life?");
+var M = " Here is a string!";
+document.write(M);
+window.alert("Enjoy!");
+let str ="Enjoy!";
+str.valueOf("Enjoy!");
+document.write(" \"I am telling on you,\" said every sibling ever "
++ "\"Not if I tell on you first\" replies the other child")
