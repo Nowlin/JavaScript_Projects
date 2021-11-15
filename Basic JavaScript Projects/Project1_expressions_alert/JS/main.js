8ba2533babeba086b9fc425c3cda98da468ed268
Nowlin <92047364+Nowlin@users.noreply.github.com>
2021-11-15 11:01:48 -0600

    Assignment Submission 1

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,15 @@
-window.alert("Hello, World!");
-document.write("How is life?");
-var M = " Here is a string!";
-document.write(M);
-window.alert("Enjoy!");
-let str ="Enjoy!";
-str.valueOf("Enjoy!");
+window.alert("Hello, World!");//A popup that greets the world
+document.write("How is life?");//A question that is written on the main page
+var M = " Here is a string!";//Defining the 'M' variable
+document.write(M);//Making the 'M' variable print on the main page
+window.alert("Enjoy!");//A popup to encourage enjoyment
+let str ="Enjoy!";"Please?";//creating a string
+str.valueOf("Enjoy!");("Please?");//defining the string
 document.write(" \"I am telling on you,\" said every sibling ever "
-+ "\"Not if I tell on you first\" replies the other child")
++ "\"Not if I tell on you first\" replies the other child. ")//A multiple "" entry written on the main page
+var Bowling = " Fun", Soccer = " Boring", Golf = " Fantastic";//creating and defining a few variables
+var Bowling = Bowling.fontcolor("green");//defining the color of the "bowling" variable
+var Soccer = Soccer.fontcolor("yellow");//defining the color of the "soccer" variable
+var Golf = Golf.fontcolor("red");//defining the color of the "golf" variable
+document.write(Golf, Bowling, Soccer);//making the variables appear on the main webpage
+document.write(1+1)//writing a mathmatical equation onto the webpage
